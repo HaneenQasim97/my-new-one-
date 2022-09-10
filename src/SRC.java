cf2d0dfbc55133abc73441c4657150481c376b02
HaneenQasim97 <haneenqassem123@gmail.com>
2022-09-10 14:40:35 +0300

    test

---
+++ src/SRC.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SRC {
+
+	public SRC() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty( "webdriver.chrome.driver", "C:\\Users\\dell\\OneDrive\\Desktop\\New folder (4)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://127.0.0.1:5500/index.html");
+		
+
+		
+List  <WebElement> items= driver.findElements(By.cssSelector(".container td:nth-of-type(3)"));
+		int sum =0;
+		
+		for(int i=0; i<items.size();i++) {
+			//System.out.println(items.get(i).getText());
+			
+	sum =sum +Integer.parseInt(items.get(i).getText());
+	//System.out.println(sum);
+	
+     	driver.findElement(By.id("total")).getText().split(":")[1].trim();
+	
+	
+	
+	
+		}
+
+	}
+
+}
